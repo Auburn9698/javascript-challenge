d520e8956efc8dabcd7a0d025fb8c98cfb1a05b6
Auburn9698 <atkingn67@yahoo.com>
2020-12-14 10:54:52 -0500

    Started js code
    Started js code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,21 @@
-// from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+// Declarations:
+// from data.js:
+var tableData = data;
+// Select the button:
+var button = d3.select("#button");
+// Select the form:
+var form = d3.select("#form");
+// Select the table body:
+var tbody = d3.select("tbody");
+
+// Create event handlers for clicking the button or pressing the enter key:
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Create the function to run for both events:
+function runEnter() {
+   // Prevent the page from refreshing:   
+    d3.event.preventDefault();
+
+}
