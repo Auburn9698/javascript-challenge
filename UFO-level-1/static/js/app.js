5601905922fec339386976214ded0fb83671fe6d
Auburn9698 <atkingn67@yahoo.com>
2020-12-14 12:53:51 -0500

    Working through app code
    Working through app code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,9 +1,8 @@
-
 // Declarations:
 // from data.js:
 var tableData = data;
 // Select the button:
-var button = d3.select("#button");
+var button = d3.select(".btn-default");
 // Select the form:
 var form = d3.select("#form");
 // Select the table body:
@@ -13,9 +12,31 @@ var tbody = d3.select("tbody");
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
+// BONUS: Refactor to use Arrow Functions!
+data.forEach((ufo_sighting) => {
+  var row = tbody.append("tr");
+  Object.entries(ufo_sighting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+
+
 // Create the function to run for both events:
 function runEnter() {
    // Prevent the page from refreshing:   
     d3.event.preventDefault();
+    // Clear table rows:
+    tbody.html("")
+    // Select the input element and get the raw HTML code:
+    var inputElement = d3.select(".form-control");
+    // Get the value property of the input element:
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    console.log(filteredData);
+
+
 
 }
